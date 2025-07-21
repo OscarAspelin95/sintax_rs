c880190906af68a5739a81e53ae9f5fa3ee30088
Oscar Aspelin <oscar.aspelin@1928diagnostics.com>
2025-07-21 15:49:53 +0200

    Revert back to slow, minimally viable approach

--- src/sintax.rs
+++ src/sintax.rs
@@ -1,76 +1,82 @@
 use crate::kmers::kmerize;
 use crate::utils::Config;
 use bio::io::fasta::Reader;
-use dashmap::DashMap;
 use rand::prelude::*;
-use rayon::prelude::*;
-use std::cmp::Reverse;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
-use std::io::BufReader;
-use std::io::Write;
+use std::io::BufWriter;
+use std::io::{BufReader, Write};
 
+/// We should probably convert hashset to vec earlier to speed up shuffling.
 pub fn bootstrap_classify_query(
     query_hashes: &HashSet<u64>,
     query_name: &str,
-    reverse_index: &DashMap<String, HashSet<u64>>,
+    reverse_index: &HashMap<u64, HashSet<String>>,
     config: &Config,
+    writer: &mut BufWriter<File>,
 ) {
     // For randomizing hashes.
     let mut rng: ThreadRng = rand::rng();
 
     // Bootstrap iterations.
     for i in 0..config.num_bootstraps {
-        // // What takes time is this step.
-        // // Randomly shuffle query hashes and take num_query_hashes first hashes.
-        // // NOTE - here we should use sampling WITH replacement, which we currently don't.
-        // let mut query_vec: Vec<&u64> = query_hashes.iter().collect();
-        // query_vec.shuffle(&mut rng);
-        // let random_hashes: HashSet<u64> = query_vec
-        //     .into_iter()
-        //     .take(config.num_query_hashes)
-        //     .map(|x| *x)
-        //     .collect();
+        // Randomly pick hashes (currently without replacement).
+        let mut query_vec: Vec<&u64> = query_hashes.iter().collect();
+        query_vec.shuffle(&mut rng);
+        let random_hashes: HashSet<u64> = query_vec
+            .into_iter()
+            .take(config.num_query_hashes)
+            .map(|x| *x)
+            .collect();
 
-        // // We have our random hashes, now we need to check which references
-        // // they match against and increment their counts.
-        // let mut count_vec: Vec<(String, usize)> = Vec::with_capacity(reverse_index.len());
+        //
 
-        // reverse_index.iter().for_each(|r| {
-        //     let ref_name = r.key();
-        //     let ref_hashes = r.value();
+        // We have our random hashes, now we need to check which references
+        // they match against and increment their counts.
 
-        //     let num_matching = ref_hashes.intersection(&random_hashes).count();
+        let mut map: HashMap<&String, usize> = HashMap::new();
 
-        //     if num_matching <= 3 {
-        //         return;
-        //     }
+        for random_hash in random_hashes {
+            match reverse_index.get(&random_hash) {
+                None => continue,
+                Some(subject_ids) => {
+                    for subject_id in subject_ids {
+                        map.entry(subject_id)
+                            .and_modify(|count| *count += 1)
+                            .or_insert(1);
+                    }
+                }
+            }
+        }
 
-        //     count_vec.push((ref_name.to_owned(), num_matching));
-        // });
+        // We need to sort HashMap by values (as vec)?
+        // Then take <top_candidates> best ones and print
+        // For now, we take the best hit.
+        let (subject_id, subject_score) = map.iter().max_by_key(|entry| entry.1).unwrap();
 
-        // // Sort by largest count.
-        // count_vec.sort_by(|a, b| Reverse(b.1).cmp(&Reverse(a.1)));
+        let result = format!(
+            "{}\t{}\t{}\t{}\n",
+            query_name,
+            subject_id,
+            subject_score,
+            i + 1
+        );
 
-        // count_vec
-        //     .iter()
-        //     .take(config.num_top_references)
-        //     .for_each(|(ref_name, count)| {
-        //         println!("{query_name}\t{ref_name}\t{i}\t{count}");
-        //         std::io::stdout().flush().unwrap();
-        //     });
+        writer.write_all(result.as_bytes()).unwrap();
     }
 }
 
 pub fn classify_queries(
     config: &Config,
-    reverse_index: &DashMap<String, HashSet<u64>>,
+    reverse_index: &HashMap<u64, HashSet<String>>,
     query_reader: Reader<BufReader<File>>,
+    writer: &mut BufWriter<File>,
 ) {
-    query_reader.records().par_bridge().for_each(|record| {
-        let r = record.unwrap();
-        let query_hashes = kmerize(&config, &r.seq());
+    query_reader.records().for_each(|record| {
+        if let Ok(r) = record {
+            let query_hashes = kmerize(&config, &r.seq());
 
-        bootstrap_classify_query(&query_hashes, &r.id(), &reverse_index, config);
+            bootstrap_classify_query(&query_hashes, &r.id(), &reverse_index, config, writer);
+        }
     });
 }
