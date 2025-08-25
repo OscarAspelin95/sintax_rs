1c32a766cbb0173cb335b20dc1731a115b4ecad2
Oscar Aspelin <130501807+OscarAspelin95@users.noreply.github.com>
2025-08-25 16:22:46 +0200

    Avoid unecessary allocations to improve runtimes

--- src/sintax/sintax.rs
+++ src/sintax/sintax.rs
@@ -30,52 +30,29 @@ fn bootstrap_classify_query(
 
     // Bootstrap iterations.
     for i in 0..config.num_bootstraps {
-        // Randomly pick hashes (currently without replacement).
-        // Not sure this is the best way to do it.
-        let random_hashes: Vec<&u64> = (0..config.num_query_hashes)
-            .map(|_| {
-                let i = rng.random_range(0..query_hashes.len());
-                query_hashes[i]
-            })
-            .collect();
+        let mut counts: Vec<usize> = vec![0; valid_records.len()];
 
-        // We have our random hashes, now we need to check which references
-        // they match against (from the reverse index) and increment their counts.
-        // NOTE - we should probably do this in some other way, such as
-        // initializing an zero-array of num_references length.
-        let mut map: FxHashMap<usize, usize> =
-            FxHashMap::with_capacity_and_hasher(10_000, FxBuildHasher);
+        for _ in 0..config.num_query_hashes {
+            let random_index = rng.random_range(0..query_hashes.len());
 
-        for random_hash in random_hashes {
-            match reverse_index.get(&random_hash) {
-                None => continue,
-                Some(bitset) => {
-                    for ref_index in bitset.ones() {
-                        map.entry(ref_index)
-                            .and_modify(|count| *count += 1)
-                            .or_insert(1);
-                    }
+            if let Some(bitset) = reverse_index.get(query_hashes[random_index]) {
+                for ref_index in bitset.ones() {
+                    counts[ref_index] += 1;
                 }
             }
         }
 
-        // We need to sort HashMap by values (as vec)?
-        // Then take <top_candidates> best ones and print
-        // For now, we take the best hit. We can probably
-        // use something better than a hashmap.
-        match map.iter().max_by_key(|entry| entry.1) {
-            Some((subject_id, subject_score)) => {
-                let result_s = format!(
-                    "{}\t{}\t{}\t{}",
-                    query_name,
-                    valid_records.get(*subject_id).unwrap().id(),
-                    subject_score,
-                    i + 1
-                );
-                iterations.push(result_s);
-            }
-            None => {}
-        };
+        if let Some((index, max_count)) = counts.iter().enumerate().max_by_key(|(_, count)| *count)
+        {
+            let result_s = format!(
+                "{}\t{}\t{}\t{}",
+                query_name,
+                valid_records[index].id(),
+                max_count,
+                i + 1
+            );
+            iterations.push(result_s);
+        }
     }
     let bootstrap_result = iterations.join("\n");
     return bootstrap_result;
@@ -86,7 +63,7 @@ pub fn classify_queries(
     reverse_index: &DashMap<u64, FixedBitSet, FxBuildHasher>,
     valid_records: &[Record],
     query_reader: Reader<BufReader<File>>,
-    writer: &Arc<Mutex<BufWriter<File>>>,
+    writer: Arc<Mutex<BufWriter<File>>>,
 ) {
     let spinner: ProgressBar = ProgressBar::new_spinner();
     spinner.enable_steady_tick(Duration::from_millis(200));
@@ -111,6 +88,9 @@ pub fn classify_queries(
             writeln!(w, "{}", bootstrap_result).unwrap()
         }
     });
+
+    let mut writer = Arc::into_inner(writer).unwrap().into_inner().unwrap();
+    writer.flush().unwrap();
 
     spinner.finish();
 }
